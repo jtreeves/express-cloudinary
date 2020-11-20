c6b210ee596dc846f739f7d4f0dfcd47680f477d
jtreeves <jackson.t.reeves@gmail.com>
2020-11-20 16:23:59 -0500

    [cloudinary] Create route and view for posting an image to the site

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-const express = require('express');
-const ejsLayouts = require('express-ejs-layouts');
-const app = express();
-const PORT = process.env.PORT || 3000;
+// Modules
+require('dotenv').config()
+const express = require('express')
+const ejsLayouts = require('express-ejs-layouts')
+const multer = require('multer')
+const cloudinary = require('cloudinary')
 
-app.set('view engine', 'ejs');
-app.use(ejsLayouts);
+// App and port
+const app = express()
+const PORT = process.env.PORT || 3000
+const uploads = multer({ dest: './uploads' })
 
+// Middleware
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+
+// Routes
 app.get('/', function(req, res) {
-  res.render('index');
-});
+    res.render('index')
+})
 
+app.post('/', uploads.single('inputFile'), (req, res) => {
+    console.log('On POST route')
+    // Get input from user
+    let file = req.file.path
+    console.log(file)
+    cloudinary.uploader.upload(file, (result) => {
+        console.log(result)
+        // Render result page with image
+        res.render('result', { image: result.url })
+    })
+})
+
+// Server listening on port
 app.listen(PORT, () => {
-  console.log(`Server running on PORT: ${PORT}`);
-});
+    console.log(`Server running on PORT: ${PORT}`)
+})
